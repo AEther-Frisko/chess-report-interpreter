ee4b2da5f0efb1efbae5abed2a94d5eda3cb505c
max <ajvwolf@gmail.com>
2025-06-30 16:30:24 -0400

    player class moved to own file, case insensitivity for player lookup

--- source.py
+++ source.py
@@ -2,53 +2,7 @@
 #   Purpose:    Reads and interprets .tms files, displaying the results
 
 import sys
-
-#   Stores all player data that is read in from a file.
-class Player:
-    def __init__(self, seed, name, cfcID, cfcRating, results):
-        self.seed = seed
-        self.name = name
-        self.cfcID = cfcID
-        self.cfcRating = cfcRating
-        self.results = results
-    
-    def __str__(self):
-        return f"Seed:       {self.seed}\
-                \nName:       {self.name}\
-                \nCFC ID:     {self.cfcID}\
-                \nCFC Rating: {self.cfcRating}\
-                \nResults:    {self.results} \n"
-    
-    #   Takes in a dict of all players.
-    #   Prints a table of round results for the player, in a
-    #   more readable format (ie. converting seed numbers to
-    #   player names).
-    def displayReport(self, players: dict):
-        print("  Round  |  Result  |  Opponent  |")
-        for result in self.results:
-            round = self.results.index(result) + 1
-            res = "?"
-            opp = "?"
-            
-            # determine result
-            if "W" in result:
-                res = "Win"
-            elif "L" in result:
-                res = "Loss"
-            elif "D" in result:
-                res = "Draw"
-            else:
-                print("This shouldn't be possible.")
-            
-            # determine opponent
-            seed = result[1:]
-            for player in players.values():
-                if player.seed == seed:
-                    opp = player.name
-
-            # display round data
-            print(f"  {round:<9}|  {res:<10}|  {opp:<12}")
-
+import player
 
 #   Takes in a filename, and populates a dict with
 #   parsed versions of each line of the file.
@@ -59,7 +13,7 @@ def readFile(filename: str):
         file = open(filename)
         for line in file:
             p = parseLine(line)
-            players[p.name] = p
+            players[p.name.lower()] = p
         file.close()
     except:
         print("ERROR: This file does not exist or cannot be found.")
@@ -79,7 +33,7 @@ def parseLine(line: str):
     results = data[4:]
     del results[-1]
 
-    p = Player(seed, name, cfcID, cfcRating, results)
+    p = player.Player(seed, name, cfcID, cfcRating, results)
     return p
 
 
@@ -100,7 +54,7 @@ while(True):
                 print(players[key])
         case 2:
             print("Please input the player's name (last, first): ")
-            key = input()
+            key = input().lower()
             if key in players.keys():
                 print(players[key])
                 players[key].displayReport(players)
