49b94d2195c565fe3cdcb127035710edbf1c7712
Peter Venables <pvenables@gmail.com>
2025-06-30 17:06:02 -0400

    some additional structure.

---
+++ src/chess_report_interpreter/tournament/player.py
@@ -0,0 +1,32 @@
+"""The player module defines individual players in a chess tournament."""
+
+from src.chess_report_interpreter.tournament.result import ChessTournamentResult
+
+
+class ChessPlayer:
+    """A class representing a player in a chess tournament."""
+
+    def __init__(
+        self,
+        seed_no: int,
+        name: str,
+        cfc_id: int,
+        cfc_rating: int,
+        results: list[ChessTournamentResult],
+    ):
+        """Initializes a Player instance."""
+        self.seed = seed_no
+        self.name = name
+        self.cfc_id = cfc_id
+        self.cfc_rating = cfc_rating
+        self.results = results
+
+    def __str__(self) -> str:
+        results_str = "\n  ".join(str(result) for result in self.results)
+        return (
+            f"Seed:       {self.seed}\n"
+            f"Name:       {self.name}\n"
+            f"CFC ID:     {self.cfc_id}\n"
+            f"CFC Rating: {self.cfc_rating}\n"
+            f"Results:\n  {results_str}\n"
+        )
---
+++ src/chess_report_interpreter/tournament/result.py
@@ -0,0 +1,43 @@
+"""Result module for chess tournament results."""
+
+from enum import Enum
+
+
+class ResultEnum(Enum):
+    """An enumeration representing the possible results of a chess game."""
+
+    WIN = "Win"
+    LOSS = "Loss"
+    DRAW = "Draw"
+
+    @classmethod
+    def from_code(cls, code: str):
+        """Converts a single character code to a ResultEnum."""
+        mapping = {"W": cls.WIN, "L": cls.LOSS, "D": cls.DRAW}
+        return mapping.get(code)
+
+    def __str__(self):
+        return self.value
+
+
+class ChessTournamentResult:
+    """A class representing the result of a chess tournament for a player."""
+
+    def __init__(self, result: str):
+        """Initializes a ChessTournamentResult instance."""
+        # Extract the result code as the first character.
+        # The rest of the string should be the opponent's seed number.
+        # Example: "W1" means a win against seed 1.
+        if result and result[0] in {"W", "L", "D"}:
+            self.result: str = result[0]
+            number_part = result[1:]
+            self.vs_seed: int = (
+                int("".join(filter(str.isdigit, number_part))) if number_part else None
+            )
+        else:
+            self.result = None
+            self.vs_seed = None
+
+    def __str__(self):
+        """Returns a string representation of the ChessTournamentResult."""
+        return f"Vs Seed: {self.vs_seed}, Result: {ResultEnum.from_code(self.result)}"
